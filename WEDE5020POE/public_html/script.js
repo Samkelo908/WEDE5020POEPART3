6a8bed15092d1ddc18056216b618a36379b3dece
Samkelo908 <129047768+Samkelo908@users.noreply.github.com>
2024-01-31 09:53:49 +0200

    Initial commit

---
+++ WEDE5020POE/public_html/script.js
@@ -0,0 +1,24 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+document.addEventListener("DOMContentLoaded", function () {
+    let slideIndex = 0;
+    showSlides();
+
+    function showSlides() {
+        let slides = document.querySelectorAll('.carousel-slide');
+        for (let i = 0; i < slides.length; i++) {
+            slides[i].style.display = 'none';
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) {
+            slideIndex = 1;
+        }
+        slides[slideIndex - 1].style.display = 'block';
+        setTimeout(showSlides, 5000); 
+    }
+});
+/*Travery Media.2017*/
+
